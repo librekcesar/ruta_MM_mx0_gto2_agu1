41aa77f45f5506e4e1cdad01b994028bce4990e0
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-07-15 11:42:08 -0600

    Add files via upload

---
+++ estaciones_mxo_gto2_agu1_copia.js
@@ -0,0 +1,10 @@
+var estaciones = {
+"type": "FeatureCollection",
+"name": "estaciones_mxo_gto2_agu1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "GTO2", "description": "GTO2", "t_estacion": "Destino" }, "geometry": { "type": "Point", "coordinates": [ -101.6429219, 21.1525125, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "AGU1", "description": "AGU1", "t_estacion": "Destino" }, "geometry": { "type": "Point", "coordinates": [ -102.2330488, 21.8641428, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "19.64220097106514, -99.14250999381147", "description": "MX0", "t_estacion": "Origen" }, "geometry": { "type": "Point", "coordinates": [ -99.1422607, 19.6422227, 0.0 ] } }
+]
+}
---
+++ paradas_mx0_gto2_agu1_copia.js
@@ -0,0 +1,14 @@
+var seguras = {
+"type": "FeatureCollection",
+"name": "paradas_mx0_gto2_agu1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Hidrosina 181", "description": "Carr. Palmillas - Apaseo el Grande km 56, 76956 Qro.", "layer": "PEMEX_MX0_AGU_GTO", "direccion": "Carr. Palmillas - Apaseo el Grande km 56, 76956 Qro." }, "geometry": { "type": "Point", "coordinates": [ -100.3924772, 20.4001054, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Hidrosina 91", "description": "Blvd. Juan Alonso de Torres Pte. 3702, San Cayetano de Medina, 37235 León de los Aldama, Gto.", "layer": "PEMEX_MX0_AGU_GTO", "direccion": "Blvd. Juan Alonso de Torres Pte. 3702, San Cayetano de Medina, 37235 León de los Aldama, Gto." }, "geometry": { "type": "Point", "coordinates": [ -101.6333055, 21.1407653, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Pemex", "description": "Av Num. 2000 Lagos de Moreno Jalisco MX 47450, Félix Ramírez Rentería, Sin Nombre, 47440 Lagos de Moreno, Jal.", "layer": "PEMEX_MX0_AGU_GTO", "direccion": "Av Num. 2000 Lagos de Moreno Jalisco MX 47450, Félix Ramírez Rentería, Sin Nombre, 47440 Lagos de Mo" }, "geometry": { "type": "Point", "coordinates": [ -101.904176, 21.370807, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Shell", "description": "Autopista México - Querétaro Km 85+880, Autopista Federal, 54250 Sabino, Méx.", "layer": "PEMEX_MX0_AGU_GTO", "direccion": "Autopista México - Querétaro Km 85+880, Autopista Federal, 54250 Sabino, Méx." }, "geometry": { "type": "Point", "coordinates": [ -99.4606279, 19.9905873, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "MexiquenseParks", "description": "MexiquenseParks, 54900, De las Torres 191, Sin Nombre, 54910 Fuentes del Valle, Méx., Mexico", "layer": "PEMEX_MX0_AGU_GTO", "direccion": "MexiquenseParks, 54900, De las Torres 191, Sin Nombre, 54910 Fuentes del Valle, Méx., Mexico" }, "geometry": { "type": "Point", "coordinates": [ -99.1422395, 19.6415736, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "GTO2", "description": "5934+2R4, León de los Aldama, Gto., Mexico", "layer": "PEMEX_MX0_AGU_GTO", "direccion": "5934+2R4, León de los Aldama, Gto., Mexico" }, "geometry": { "type": "Point", "coordinates": [ -101.6429219, 21.1525125, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "AGU1", "description": "Carmen de Luna de Sevilla 1509, José Guadalupe Peralta Gámez, 20196 Aguascalientes, Ags., Mexico", "layer": "PEMEX_MX0_AGU_GTO", "direccion": "Carmen de Luna de Sevilla 1509, José Guadalupe Peralta Gámez, 20196 Aguascalientes, Ags., Mexico" }, "geometry": { "type": "Point", "coordinates": [ -102.2330488, 21.8641428, 0.0 ] } }
+]
+}
